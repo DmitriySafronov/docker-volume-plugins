26c5781d22cf341367283fb2fd86db5a152ccba6
Dmitriy Safronov <DmitriySafronov@users.noreply.github.com>
2019-05-01 10:16:08 +0300

    Reconfigure build

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 TAG=$1
 build() {
-    docker plugin rm -f scrunter/$1 || true
+    docker plugin rm -f zimniy/$1 || true
     docker rmi -f rootfsimage || true
     docker build -t rootfsimage $1
     id=$(docker create rootfsimage -h) # id was cd851ce43a403 when the image was created
@@ -12,10 +12,10 @@ build() {
     cp $1/config.json build
     if [ -z "$TAG" ]
     then
-        docker plugin create scrunter/$1 build
+        docker plugin create zimniy/$1 build
     else
-        docker plugin create scrunter/$1:$TAG build
-        docker plugin push scrunter/$1:$TAG
+        docker plugin create zimniy/$1:$TAG build
+        docker plugin push zimniy/$1:$TAG
     fi
 }
 build glusterfs-volume-plugin
